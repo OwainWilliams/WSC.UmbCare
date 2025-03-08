48d7ea52e8ea291adbede12cba13e5e41e5ac432
Owain Williams <owaingdwilliams@gmail.com>
2025-03-08 21:23:42 +0000

    Bluescreen toggle update

--- src/WSC.UmbCare/Client/src/bluescreen/bluescreen-element.ts
+++ src/WSC.UmbCare/Client/src/bluescreen/bluescreen-element.ts
@@ -5,7 +5,7 @@ import { UmbElementMixin } from "@umbraco-cms/backoffice/element-api";
 @customElement('bluescreen-element')
 
 export default class BluescreenElement extends UmbElementMixin(LitElement) {
-
+  private _isBluescreenOn = false;
   connectedCallback() {
     super.connectedCallback();
 
@@ -14,7 +14,7 @@ export default class BluescreenElement extends UmbElementMixin(LitElement) {
   private _triggerBluescreen = () => {
     console.log("bluescreen button");
     const body = document.body;
-    if (body.style.filter === 'hue-rotate(180deg)') {
+    if (this._isBluescreenOn) {
       body.style.filter = '';
       body.style.backgroundColor = '';
       body.style.color = '';
@@ -23,23 +23,19 @@ export default class BluescreenElement extends UmbElementMixin(LitElement) {
       body.style.backgroundColor = 'blue';
       body.style.color = 'white';
     }
+    this._isBluescreenOn = !this._isBluescreenOn;
+    this.requestUpdate();
   }
 
 
   render() {
     return html`
-   <uui-button look="primary" color="positive" id="blueScreen" label="Open" @click="${this._triggerBluescreen}">Bluescreen</uui-button>
+   <uui-button look="primary" color="positive" id="blueScreen" label="Open" @click="${this._triggerBluescreen}">
+         ${this._isBluescreenOn ? 'Bluescreen On' : 'Bluescreen Off'}
+   </uui-button>
     `;
   }
 
-//  static styles = [
-//    css`
-//    body.blue-screen {
-//      filter: hue-rotate(180deg);
-//      background-color: blue;
-//      color: white;
-//}
-//    `];
 }
 
 declare global {
