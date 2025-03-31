48e46f8211bdcb9654f8062d45fdea90a2883fe0
Owain Williams <owaingdwilliams@gmail.com>
2025-03-31 11:28:55 +0100

    Fix modal close button

--- src/WSC.UmbCare/Client/src/headerapp/headerapp-modal.element.ts
+++ src/WSC.UmbCare/Client/src/headerapp/headerapp-modal.element.ts
@@ -1,18 +1,37 @@
 import { customElement, html } from "@umbraco-cms/backoffice/external/lit";
 import { UmbModalBaseElement } from "@umbraco-cms/backoffice/modal";
 import { HeaderAppModalData, HeaderAppModalValue } from "../headerapp/headerapp-modal.token";
-import "./headerapp";
 
 @customElement('headerapp-modal')
 export class headerappModal extends UmbModalBaseElement<HeaderAppModalData, HeaderAppModalValue>{
-
- 
-  render() {
-    return html`
-   <headerapp-element></headerapp-element>
-
-    `;
+    constructor() {
+    super();
   }
 
+  connectedCallback() {
+    super.connectedCallback();
+  }
+
+  override render() {
+    return html`
+    <umb-body-layout headline="It's a modal, but not as you know it!">
+				<uui-box>
+        <uui-toggle pristine="" label="Disable Countdown"></uui-toggle>
+        <br />
+        <uui-toggle pristine="" label="Blue light filter"></uui-toggle>
+        </uui-box>
+        	<div slot="actions">
+            <uui-button id="close" label="Close" @click="${this._rejectModal}" look="secondary" color="default">Close</uui-button>
+				</div>
+      </umb-body-layout>
+        `;
+  }
 }
+
 export default headerappModal;
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'headerapp-model': headerappModal;
+  }
+}
